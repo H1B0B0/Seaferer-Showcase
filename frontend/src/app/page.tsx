0c8b77a9087c23c181b6a2ce6e7a373bcae594cf
H1B0B0 <hibosmurf@gmail.com>
2024-11-05 16:59:01 +0100

    feat: Set up Next.js project with Tailwind CSS and Spline integration; remove unused files and configurations

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Spline from "@splinetool/react-spline";
+
+export default function Home() {
+  return (
+    <main className="w-full h-full">
+      <Spline
+        scene="https://prod.spline.design/A6geAl3g2ozafJjK/scene.splinecode"
+        className="absolute w-full h-full"
+      />
+    </main>
+  );
+}
