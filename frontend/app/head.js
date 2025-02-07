c9303bb47d004513da4166a43d1990a9812a9770
Etienne Mentrel <etienne.mentrel@scafe.io>
2025-02-07 11:36:13 +0100

    refactor: Update Head component metadata for improved SEO; remove unused API route

--- frontend/app/head.js
+++ frontend/app/head.js
@@ -1,8 +1,37 @@
 const Head = () => (
   <>
-    <title>Seaferer</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" href="/seaferer-fav.png" />
+    <title>Seafarer Cloud</title>
+    <meta
+      name="description"
+      content="Seaferer is an automated tool to quickly deploy your cloud infrastructure with a guided quiz and a user-friendly interface."
+    />
+    <meta name="robots" content="index, follow" />
+    <meta
+      name="keywords"
+      content="cloud, automation, infrastructure, devops, rapid deployment, guided quiz"
+    />
+    <meta property="og:title" content="Seafarer Cloud" />
+    <meta
+      property="og:description"
+      content="Seaferer is an automated tool to quickly deploy your cloud infrastructure with a guided quiz and a user-friendly interface."
+    />
+    <meta property="og:url" content="https://seaferver.com/" />
+    <meta property="og:type" content="website" />
+    <meta
+      property="og:image"
+      content="https://seaferver.com/seaferer-fav.png"
+    />
+    <link rel="canonical" href="https://seaferver.com/" />
+    <script type="application/ld+json">
+      {`
+        {
+          "@context": "https://schema.org",
+          "@type": "WebSite",
+          "name": "Seafarer Cloud",
+          "url": "https://seaferver.com/"
+        }
+      `}
+    </script>
   </>
 );
 
